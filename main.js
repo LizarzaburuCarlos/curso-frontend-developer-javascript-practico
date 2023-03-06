66646f25ad10460d6c1c1fa7b51dc75435951bf3
LizarzaburuCarlos <carlos_abanto2@hotmail.com>
2023-03-06 00:23:43 -0500

    Mobile Menu

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail.addEventListener('click',toggleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
-    console.log("muerte");
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
